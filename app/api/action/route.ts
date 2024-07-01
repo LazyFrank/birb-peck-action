603f7e289de4f4af962e99860de00cdc1f490286
Don Medlinger <don@kongarian.app>
2024-06-30 23:14:34 -0700

    Updates for testing

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -2,10 +2,9 @@ import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
+  // TODO
   const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body, {
-    neynarApiKey: process.env.NEYNAR_ONCHAIN_KIT,
-  });
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   if (!isValid) {
     return new NextResponse('Message not valid', { status: 500 });
