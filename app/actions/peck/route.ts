f78ffe6c5f807be5c24f50882a56ed99cbb2e8c4
LazyFrank <don.medlinger@gmail.com>
2024-07-12 22:44:06 -0700

    Update URL

--- app/actions/peck/route.ts
+++ app/actions/peck/route.ts
@@ -3,15 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import { LAZYFRANK_URL } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const body: FrameRequest = await req.json();
-  const { isValid } = await getFrameMessage(body, {
-    neynarApiKey: process.env.NEYNAR_KEY,
-  });
-
-  if (!isValid) {
-    return new NextResponse('Message not valid', { status: 500 });
-  }
-
   return NextResponse.json(
     { type: 'frame', frameurl: `${LAZYFRANK_URL}/frames/peck` },
     { status: 200 },
